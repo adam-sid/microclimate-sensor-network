bdbab9a42cf583ba8932c598eefe810a3f03d4f7
adam-sid <148649495+adam-sid@users.noreply.github.com>
2025-05-29 13:43:52 +0100

    update home page message

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 // Writing out the types for request and response just for clarity
 // In reality, typescriopt can infer these types automatically
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello from Express + TypeScript!!!!');
+  res.send('Placeholder for home page');
 });
 
 app.get('/subdirectory', (req: Request, res: Response) => {
