cc1a4ec389589b62af7df35a5c5db1f353778ed2
adam-sid <148649495+adam-sid@users.noreply.github.com>
2025-08-14 16:47:06 +0100

    update front end to pull from new database

--- public/fetchData.js
+++ public/fetchData.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   document.querySelector('#location').insertAdjacentText('beforeend', "Chipping Sodbury")
 
-  fetch('/api/chipping-sodbury?column=*&device_id=1')
+  fetch('/api/database/select-latest?requestedTable=node_data&selectColumn=*&conditionColumn=node_name&value=1')
     .then(res => {
       return res.json();
     })
@@ -12,13 +12,13 @@ document.addEventListener('DOMContentLoaded', () => {
       document.querySelector('#humidity-1').insertAdjacentText('afterbegin', data.humidity)
       document.querySelector('#wind-speed-1').insertAdjacentText('afterbegin', data.wind_speed)
       document.querySelector('#soil-moisture-1').insertAdjacentText('afterbegin', data.soil_moisture)
-      const britishTime = new Date(data.timestamp).toLocaleString('en-GB', { timeZone: 'Europe/London' });
+      const britishTime = new Date(data.ts * 1000).toLocaleString('en-GB', { timeZone: 'Europe/London' });
       document.querySelector('#timestamp-1').insertAdjacentText('beforeend', britishTime)
       document.querySelector('#gust-speed-1').insertAdjacentText('beforeend', data.gust_speed + ' m/s')
     })
     .catch(error => console.log(error));
 
-  fetch('/api/chipping-sodbury?column=*&device_id=2')
+  fetch('/api/database/select-latest?requestedTable=node_data&selectColumn=*&conditionColumn=node_name&value=2')
     .then(res => {
       return res.json();
     })
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
       document.querySelector('#humidity-2').insertAdjacentText('afterbegin', data.humidity)
       document.querySelector('#wind-speed-2').insertAdjacentText('afterbegin', data.wind_speed)
       document.querySelector('#soil-moisture-2').insertAdjacentText('afterbegin', data.soil_moisture)
-      const britishTime = new Date(data.timestamp).toLocaleString('en-GB', { timeZone: 'Europe/London' });
+      const britishTime = new Date(data.ts * 1000).toLocaleString('en-GB', { timeZone: 'Europe/London' });
       document.querySelector('#timestamp-2').insertAdjacentText('beforeend', britishTime)
       document.querySelector('#gust-speed-2').insertAdjacentText('beforeend', data.gust_speed + ' m/s')
     })
